263b24bc3eb61d265ad8229aa5f5ad3a4e99b10b
Solst1tium <solange.leiva@mayor.cl>
2020-09-05 19:22:22 -0400

    api nasa agregando vuetify

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import firebase from 'firebase'
 import firebaseConfig from '../config/firebase'
+import vuetify from './plugins/vuetify';
 
 firebase.initializeApp(firebaseConfig)
 
@@ -13,9 +14,10 @@ let app;
 firebase.auth().onAuthStateChanged(() => {
   if(!app){
     app = new Vue({
-    router,
-    store,
-    render: h => h(App)
-  }).$mount('#app') 
+      router,
+      store,
+      vuetify,
+      render: h => h(App)
+    }).$mount('#app') 
   }
 })
